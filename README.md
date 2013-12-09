c4437e8bd9c723d4a0c81fd3c3812fc5c50c6a0e
Alex Mingoia <talk@alexmingoia.com>
2013-12-08 18:15:19 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,118 @@
+# mio-mysql
+
+[![Build Status](https://secure.travis-ci.org/alexmingoia/mio-mysql.png?branch=master)](http://travis-ci.org/alexmingoia/mio-mysql)
+[![Dependency Status](https://david-dm.org/alexmingoia/mio-mysql.png)](http://david-dm.org/alexmingoia/mio-mysql)
+
+MySQL persistence layer for [Mio][0].
+
+## Installation
+
+```sh
+npm install mio-mysql
+```
+
+## API
+
+See the [Mio][0] documentation for query methods like find, save, etc.
+
+### exports(settings, options)
+
+Create a new Mio plugin with the given database `settings` and `options`.
+
+`settings` same as settings for
+[node-mysql](https://github.com/felixge/node-mysql/)
+
+`options`
+* `tableName` The table for this model. Defaults to singularized model name.
+* `maxLimit` The maximum number of records to select at once. Default is 200.
+
+```javascript
+var mio = require('mio');
+
+var User = mio.createModel('User');
+
+User.use('server', 'mio-mysql', {
+  database: 'mydb',
+  user: 'root'
+});
+```
+
+### Queries
+
+The query is a subset of [mongo-sql](https://github.com/goodybag/mongo-sql).
+The `type`, `columns`, and `table` properties are handled by mio-mysql.
+
+### Custom table / field names
+
+Custom table names are specified using the `tableName` option. For example:
+
+```javascript
+User.use(mysql({
+  database: 'mydb',
+  user: 'root'
+}, {
+  tableName: 'users'
+}));
+```
+
+Custom field names are provided by a `columnName` property in the attribute
+definition. For example:
+
+```javascript
+User
+  .attr('id')
+  .attr('firstName', {
+    type: 'string',
+    length: 255,
+    columnName: 'first_name'
+  })
+  .attr('lastName', {
+    type: 'string',
+    length: 255,
+    columnName: 'last_name'
+  });
+```
+
+### Date types
+
+Attributes with `type: "date"` will be handled based on the `columnType`
+property. This property can either be "datetime", "timestamp", or "integer",
+corresponding to MySQL column type.
+
+### Data formatters
+
+If you need to control exactly how a data-type is determined, set the attribute
+definition's `dataFormatter` function:
+
+```javascript
+var Event = mio.createModel('Event');
+
+Event.attr('date', { dataFormatter: function(value, Event) {
+  value = Math.floor(value.getTime() / 1000);
+  return value;
+});
+```
+
+### Connection pooling
+
+Models that share a settings object will share a connection pool, exposed via
+`settings.pool`.
+
+### exports.mysql
+
+[MySQL](https://github.com/felixge/node-mysql) module.
+
+## Tests
+
+Tests are written with [mocha](https://github.com/visionmedia/mocha) and
+[should](https://github.com/visionmedia/should.js) using BDD-style assertions.
+
+Run the tests with npm:
+
+```sh
+npm test
+```
+
+## MIT Licensed
+
+[0]: https://github.com/alexmingoia/mio/
---
+++ test/mysql.js
@@ -0,0 +1,330 @@
+/**
+ * mio-mysql tests.
+ */
+
+var should = require('should');
+var mio = require('mio');
+var mysql = require('..');
+
+var settings = {};
+
+mysql.adapter.createPool = function(settings) {
+  return {
+    on: function(event, callback) {},
+    end: function() {},
+    query: function(statement, values, done) {
+      done(null, [], {});
+    }
+  };
+};
+
+describe('module', function(done) {
+  it('exports plugin factory', function(done) {
+    should.exist(mysql);
+    mysql.should.be.a('function');
+    done();
+  });
+
+  it('constructs new mysql plugins', function(done) {
+    var plugin = mysql, settings;
+    should.exist(plugin);
+    plugin.should.be.a('function');
+    done();
+  });
+});
+
+describe('plugin', function() {
+  it('exposes db connection on model', function(done) {
+    var User = mio.createModel('User').attr('id').attr('name');
+    User.use(mysql, settings);
+    should.exist(User.adapter.db);
+    done();
+  });
+});
+
+describe('Model', function() {
+  var User, Post;
+
+  beforeEach(function(done) {
+    User = mio.createModel('User').attr('id', { primary: true }).attr('name');
+    Post = mio.createModel('Post').attr('id', { primary: true }).attr('title').attr('user_id');
+    User.use(mysql, settings);
+    Post.use(mysql, settings);
+    done();
+  });
+
+  describe('.findAll()', function() {
+    it('finds all models successfully', function(done) {
+      var userA = new User({id: 1, name: 'alex'});
+      var userB = new User({id: 2, name: 'jeff'});
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, callback) {
+        statement.sql.should.include(
+          'from "user" where "user"."id" = $1 or "user"."name" = $2 limit $3'
+        );
+        for (var key in userA.attributes) {
+          userA.attributes[User.options.tableName + '_' + key] = userA.attributes[key];
+        }
+        for (var key in userB.attributes) {
+          userB.attributes[User.options.tableName + '_' + key] = userB.attributes[key];
+        }
+        callback(null, [userA.attributes, userB.attributes], userB.attributes);
+      };
+      User.all(
+        { where: { $or: { id: userA.primary, name: "jeff" }}},
+        function(err, found) {
+          User.adapter.db.query = query;
+          if (err) return done(err);
+          should.exist(found);
+          found.should.be.instanceOf(Array);
+          found.should.have.property('limit', 50);
+          found.should.have.property('offset');
+          found.pop().primary.should.equal(userB.primary);
+          done();
+        }
+      );
+    });
+
+    it('passes errors to callback', function(done) {
+      var user = new User({ name: 'alex' });
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, callback) {
+        callback(new Error('error finding users.'));
+      };
+      User.all(
+        { where: { $or: { id: user.primary, name: "alex" }}},
+        function(err, found) {
+          User.adapter.db.query = query;
+          should.exist(err);
+          err.should.have.property('message', 'error finding users.');
+          done();
+        }
+      );
+    });
+
+    it("uses attribute definition's columnName in queries", function(done) {
+      User = mio.createModel('User')
+        .attr('id', { primary: true })
+        .attr('fullname', {
+          type: 'string',
+          length: 255,
+          columnName: 'name'
+        });
+      User.use(mysql, settings);
+      var user = new User({ fullname: 'alex' });
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        User.adapter.db.query = query;
+        statement.sql.should.equal(
+          'insert into "user" ("name") values ($1)'
+        );
+        cb(null, { insertId: 1 }, {});
+      };
+      user.save(function(err) {
+        if (err) return done(err);
+        user.should.have.property('fullname');
+        user.fullname.should.equal('alex');
+        done();
+      });
+    });
+  });
+
+  describe('.find()', function() {
+    it('finds model by id successfully', function(done) {
+      var user = new User({id: 1, name: 'alex'});
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        statement.sql.should.equal(
+          'select "user".* from "user" where "user"."id" = $1'
+        );
+        User.adapter.db.query = query;
+        for (var key in user.attributes) {
+          user.attributes[User.options.tableName + '_' + key] = user.attributes[key];
+        }
+        cb(null, [user.attributes], {});
+      };
+      User.find(user.primary, function(err, found) {
+        if (err) return done(err);
+        should.exist(found);
+        user.primary.should.equal(found.primary);
+        done();
+      });
+    });
+
+    it('passes errors to callback', function(done) {
+      var user = new User({ name: 'alex' });
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, callback) {
+        callback(new Error('error finding user.'));
+      };
+      User.find(user.primary, function(err, found) {
+        User.adapter.db.query = query;
+        should.exist(err);
+        err.should.have.property('message', 'error finding user.');
+        done();
+      });
+    });
+  });
+
+  describe('.count()', function() {
+    it('counts models successfully', function(done) {
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        statement.sql.should.equal(
+          'select COUNT(*) as _count from "user" where "user"."name" = $1'
+        );
+        cb(null, [{_count: 3}], {});
+      };
+      User.count({ name: 'alex' }, function(err, count) {
+        User.adapter.db.query = query;
+        if (err) return done(err);
+        should.exist(count);
+        count.should.equal(3);
+        done();
+      });
+    });
+
+    it('passes errors to callback', function(done) {
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        cb(new Error("error removing all models."));
+      };
+      User.count({ name: 'alex' }, function(err) {
+        User.adapter.db.query = query;
+        should.exist(err);
+        err.should.have.property('message', 'error removing all models.');
+        done();
+      });
+    });
+  });
+
+  describe('.removeAll()', function() {
+    it('removes models successfully', function(done) {
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        statement.sql.should.equal(
+          'delete from "user" where "user"."name" = $1'
+        );
+        cb(null, {}, {});
+      };
+      User.removeAll({ name: 'alex' }, function(err) {
+        User.adapter.db.query = query;
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('passes errors to callback', function(done) {
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        cb(new Error("error removing all models."));
+      };
+      User.removeAll({ name: 'alex' }, function(err) {
+        User.adapter.db.query = query;
+        should.exist(err);
+        err.should.have.property('message', 'error removing all models.');
+        done();
+      });
+    });
+  });
+
+  describe('#save()', function() {
+    it('saves new model successfully', function(done) {
+      var user = new User({name: 'alex'});
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        User.adapter.db.query = query;
+        values.should.include('alex');
+        cb(null, { insertId: 1 }, {});
+      };
+      user.save(function(err) {
+        should.not.exist(err);
+        should.exist(user.primary);
+        done();
+      });
+    });
+
+    it('passes errors to callback', function(done) {
+      var user = new User({ name: 'alex' });
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, callback) {
+        User.adapter.db.query = query;
+        callback(new Error('error saving user.'));
+      };
+      user.save(function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'error saving user.');
+        done();
+      });
+    });
+  });
+
+  describe('#update()', function() {
+    it('updates model successfully', function(done) {
+      var user = new User({id: 1, name: 'alex'});
+      user.dirtyAttributes.length = 0;
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        User.adapter.db.query = query;
+        statement.sql.should.equal(
+          'update "user" set "name" = $1 where "user"."id" = $2'
+        );
+        values.should.include('jeff', 1);
+        cb(null, [user], user.attributes);
+      };
+      user.name = 'jeff';
+      user.save(function(err) {
+        should.not.exist(err);
+        user.name.should.equal('jeff');
+        done();
+      });
+    });
+
+    it('passes errors to callback', function(done) {
+      var user = new User({ name: 'alex' });
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, callback) {
+        callback(new Error('error updating user.'));
+      };
+      user.save(function(err) {
+        User.adapter.db.query = query;
+        should.exist(err);
+        err.should.have.property('message', 'error updating user.');
+        done();
+      });
+    });
+  });
+
+  describe('#remove', function() {
+    it('removes model successfully', function(done) {
+      var user = new User({id: 1, name: 'alex'});
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, cb) {
+        User.adapter.db.query = query;
+        statement.sql.should.equal(
+          'delete from "user" where "user"."id" = $1'
+        );
+        values.should.include(1);
+        cb(null, [], {});
+      };
+      user.remove(function(err) {
+        should.not.exist(err);
+        done();
+      });
+    });
+
+    it('passes errors to callback', function(done) {
+      var user = new User({ id: 1, name: 'alex' });
+      var query = User.adapter.db.query;
+      User.adapter.db.query = function(statement, values, callback) {
+        User.adapter.db.query = query;
+        callback(new Error('error removing user.'));
+      };
+      user.remove(function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'error removing user.');
+        done();
+      });
+    });
+  });
+});
